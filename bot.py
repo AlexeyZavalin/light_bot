8404a143e9ba1b8ca306e3f85a3f6235d48e5e66
Alexey Zavalin <lehazavalin95@gmail.com>
2026-01-14 21:49:44 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,99 @@
+from os import getenv
+
+from paho.mqtt import client as mqtt
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes
+)
+
+# ================= MQTT =================
+MQTT_BROKER = getenv("MQTT_BROKER")
+MQTT_PORT = getenv("MQTT_PORT")
+MQTT_TOPIC = getenv("MQTT_TOPIC")
+MQTT_USERNAME = getenv("MQTT_USERNAME")
+MQTT_PASSWORD = getenv("MQTT_PASSWORD")
+MQTT_CA_FILE = getenv("MQTT_CA_FILE")
+
+mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqtt_client.tls_set()
+mqtt_client.tls_insecure_set(True)
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("✅ MQTT connected")
+        client.subscribe(MQTT_TOPIC, qos=1)
+    else:
+        print(f"❌ MQTT connect failed rc={rc}")
+
+mqtt_client.on_connect = on_connect
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
+mqtt_client.loop_start()
+
+# ================= TELEGRAM =================
+
+async def colors(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [
+            InlineKeyboardButton("🔴-", callback_data="2"),
+            InlineKeyboardButton("🔵+", callback_data="3"),
+            InlineKeyboardButton("➕", callback_data="4"),
+            InlineKeyboardButton("➖", callback_data="5"),
+            
+        ],
+        [
+            InlineKeyboardButton("🔅", callback_data="6"),
+            InlineKeyboardButton("🔆", callback_data="7"),
+            InlineKeyboardButton("🎨", callback_data="8"),
+            InlineKeyboardButton("🌊", callback_data="9"),
+        ],
+        [
+            InlineKeyboardButton("🌈", callback_data="10"),
+            InlineKeyboardButton("🔥", callback_data="11"),
+            InlineKeyboardButton("❤️‍🔥", callback_data="12"),
+            InlineKeyboardButton("🕺", callback_data="13"),
+        ],
+        [
+            InlineKeyboardButton("☄️", callback_data="14"),
+            InlineKeyboardButton("✨", callback_data="15"),
+            InlineKeyboardButton("🌠", callback_data="16"),
+            InlineKeyboardButton("🌑/💡", callback_data="1"),
+        ]
+    ]
+
+    await update.message.reply_text(
+        "🎨 Выбери цвет ленты:",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+async def color_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    await query.answer("🎨 Отправляю цвет…", show_alert=False)
+
+    payload = query.data
+
+    mqtt_client.publish(MQTT_TOPIC, payload, qos=1)
+
+# ================= MAIN =================
+
+def main():
+    app = ApplicationBuilder().token(
+        "8078129399:AAG-2FzohoPoU2Wv9UAFE418ybUTW3zJDJo"
+    ).build()
+
+    app.add_handler(CommandHandler("start", colors))
+    app.add_handler(CallbackQueryHandler(color_callback))
+
+    print("🤖 Telegram bot started")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
